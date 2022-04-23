a90265a2e5f8c2a6cd7e41347fe7a3e390372089
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-04-23 11:14:21 +0800

    conditional rendering

--- chapter2-theEssentials/public/app.js
+++ chapter2-theEssentials/public/app.js
@@ -1,47 +1,36 @@
-const root = document.querySelector('#root'); //Component lifecycle (effect hooks)
+const root = document.querySelector('#root'); //Conditional rendering
 
 /**
- * useEffect adalah sebuah function yang akan dipanggil jika sebuah component dirender/ di re-render
- * useEffect akan terus dipanggil jika component melakukan perubahan
- * akan tetapi, useEffect bisa dipanggil hanya dengan memperhatikan perubahan salah satu state saja,
- * dengan menambahkan di parameter ke-2 array yang berisi state yang terkait
- * Array di parameter ke-2, bisa berupa array kososng, sehingga useEffect akan dipanggil sekali saja ketika pertama kali render
+ * Conditional rendering memungkinkan kita untuk merender sebuah component
+ * dengan kondisi tertentu.
  */
 
 function App() {
-  const [diklik, setDiklik] = React.useState(false);
-  const [count, setCount] = React.useState(0);
-  React.useEffect(() => {
-    console.log(document.getElementById('judul'));
-  }, [count] //bisa diisi oleh dua atau lebih state / tidak sama sekali
-  );
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    id: "judul"
-  }, "Hello, saya judul"), /*#__PURE__*/React.createElement("button", {
+  const [login, setLogin] = React.useState(false); //jika ingin menggunakan block if
+  // if (login) {
+  //   return (
+  //     <>
+  //       <h1>Login Successfull.</h1>
+  //       <button
+  //       onClick={() => {
+  //         setLogin(false)
+  //       }}
+  //     >
+  //       Login
+  //     </button>
+  //     </>
+  //   )
+  // }
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Application"), /*#__PURE__*/React.createElement("p", null, login ? /*#__PURE__*/React.createElement("b", null, "Sudah Login") : /*#__PURE__*/React.createElement("i", null, "Belum Login")), /*#__PURE__*/React.createElement("button", {
     onClick: () => {
-      setDiklik(true);
+      if (login) {
+        setLogin(false);
+      } else {
+        setLogin(true);
+      }
     }
-  }, "Click Me"), /*#__PURE__*/React.createElement("button", {
-    onClick: () => {
-      setCount(count + 1);
-    }
-  }, "Tambah"), /*#__PURE__*/React.createElement("span", null, ": ", count));
+  }, "Login"));
 }
 
-setTimeout(() => {
-  ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
-}, 2000);
-/**
-//console.log, bisa menghasilkan value karena component sudah dirender
-ReactDOM.render(<App />, root)
-console.log(document.getElementById('judul'));
- */
-
-/** 
-//console.log, memiliki value null karena render dilakukan setelah 2second
-// bagaimana cara mendapatkan value element dalam kasus dibawah ini, adalah menggunakan useEffect 
-setTimeout(function () {
-  ReactDOM.render(<App />, root)
-}, 2000);
-console.log(document.getElementById('judul'));
-*/
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
