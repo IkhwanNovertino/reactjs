1757559412988da7dabd4f52609383c88bb25c73
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-04-23 12:23:17 +0800

    react list & keys(render multiple components

--- chapter2-theEssentials/public/app.js
+++ chapter2-theEssentials/public/app.js
@@ -1,36 +1,25 @@
-const root = document.querySelector('#root'); //Conditional rendering
-
-/**
- * Conditional rendering memungkinkan kita untuk merender sebuah component
- * dengan kondisi tertentu.
- */
+const root = document.querySelector('#root'); //DOM Manipulation (Ref Hooks)
 
 function App() {
-  const [login, setLogin] = React.useState(false); //jika ingin menggunakan block if
-  // if (login) {
-  //   return (
-  //     <>
-  //       <h1>Login Successfull.</h1>
-  //       <button
-  //       onClick={() => {
-  //         setLogin(false)
-  //       }}
-  //     >
-  //       Login
-  //     </button>
-  //     </>
-  //   )
-  // }
+  const judulRef = React.useRef(null); // Manipulasi DOM manual
+  // React.useEffect(() => {
+  //   const judul = document.getElementById('judul')
+  //   console.log(judul);
+  //   setTimeout(() => {
+  //     judul.textContent= 'Web Page'
+  //   }, 2000);
+  // })
+  // Manipulasi DOM menggunakan  Ref Hooks
 
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Application"), /*#__PURE__*/React.createElement("p", null, login ? /*#__PURE__*/React.createElement("b", null, "Sudah Login") : /*#__PURE__*/React.createElement("i", null, "Belum Login")), /*#__PURE__*/React.createElement("button", {
-    onClick: () => {
-      if (login) {
-        setLogin(false);
-      } else {
-        setLogin(true);
-      }
-    }
-  }, "Login"));
+  React.useEffect(() => {
+    console.log(judulRef);
+    setTimeout(() => {
+      judulRef.current.textContent = 'Web Page';
+    }, 2000);
+  });
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    ref: judulRef
+  }, "Application"));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
