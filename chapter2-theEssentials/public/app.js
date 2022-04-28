da2c096fdd13cb60320c2a0a2f7f301734e5b4f4
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-04-28 22:04:07 +0800

    create todo list app

--- chapter2-theEssentials/public/app.js
+++ chapter2-theEssentials/public/app.js
@@ -1,26 +1,98 @@
-const root = document.querySelector('#root'); // Data fetch
+const root = document.querySelector('#root'); // Todo List
 
 function App() {
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(true); //Data fetch menggunakan async await
+  const [activity, setActivity] = React.useState('');
+  const [edit, setEdit] = React.useState({});
+  const [todos, setTodos] = React.useState([]);
+  const [msg, setMsg] = React.useState('');
 
-  React.useEffect(() => {
-    const getData = async () => {
-      const req = await fetch('https://api.spaceflightnewsapi.net/v3/blogs'); // console.log(req);
+  const generateId = () => Date.now();
 
-      const res = await req.json(); // console.log(res);
+  const saveTodoHandler = event => {
+    event.preventDefault();
+    if (!activity) return setMsg('Nama Aktifitas harus diisi');
+    setMsg('');
 
-      setNews(res);
-      setLoading(false);
+    if (edit.id) {
+      const updatedTodo = { ...edit,
+        activity,
+        done: false
+      };
+      const todoIndex = todos.findIndex(todo => todo.id === edit.id);
+      const updatedTodos = [...todos];
+      updatedTodos[todoIndex] = updatedTodo;
+      setTodos(updatedTodos);
+      return cancelEditHandler();
+    }
+
+    setTodos([...todos, {
+      id: generateId(),
+      activity,
+      done: false
+    }]);
+    setActivity(''); // console.log(todos);
+  };
+
+  const editTodoHandler = todo => {
+    // console.log(todo);
+    setEdit(todo);
+    setActivity(todo.activity);
+  };
+
+  const doneTodoHandler = todo => {
+    const updatedTodo = { ...todo,
+      done: todo.done ? false : true
     };
+    const todoIndex = todos.findIndex(currentTodo => currentTodo.id === todo.id);
+    const updatedTodos = [...todos];
+    updatedTodos[todoIndex] = updatedTodo;
+    setTodos(updatedTodos);
+  };
 
-    getData();
-  });
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), loading ? /*#__PURE__*/React.createElement("i", null, "Loading Data ...") : /*#__PURE__*/React.createElement("ol", null, news.map(item => {
+  const removeTodoHandler = todoId => {
+    // console.log('Hapus todo dengan id:', todoId);
+    const filteredTodos = todos.filter(todo => {
+      return todo.id !== todoId;
+    });
+    setTodos(filteredTodos);
+    if (edit.id) cancelEditHandler();
+  };
+
+  const cancelEditHandler = () => {
+    setEdit({});
+    setActivity('');
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple Todo List"), msg && /*#__PURE__*/React.createElement("p", {
+    style: {
+      color: 'red'
+    }
+  }, msg), /*#__PURE__*/React.createElement("form", {
+    onSubmit: saveTodoHandler
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Nama Aktifikat...",
+    value: activity,
+    onChange: event => {
+      setActivity(event.target.value);
+    }
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, edit.id ? 'Simpan perubahan' : 'Tambah'), edit.id && /*#__PURE__*/React.createElement("button", {
+    onClick: cancelEditHandler
+  }, "Batal")), todos.length > 0 ? /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
-      key: item.id
-    }, item.title);
-  })));
+      key: todo.id
+    }, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      checked: todo.done,
+      onChange: doneTodoHandler.bind(this, todo)
+    }), todo.activity, "(", todo.done ? 'Selesai' : 'Belum Selesai', ")", /*#__PURE__*/React.createElement("button", {
+      onClick: editTodoHandler.bind(this, todo)
+    }, "Edit"), /*#__PURE__*/React.createElement("button", {
+      onClick: removeTodoHandler.bind(this, todo.id)
+    }, "Hapus"));
+  })) : /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("i", null, "Tidak ada todo")));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- chapter2-theEssentials/src/app.js
+++ chapter2-theEssentials/src/app.js
@@ -1,34 +1,125 @@
 const root = document.querySelector('#root');
-// Data fetch
+// Todo List
 function App() {
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
-  //Data fetch menggunakan async await
-  React.useEffect(() => {
-    const getData = async () => {
-      const req = await fetch(
-        'https://api.spaceflightnewsapi.net/v3/blogs'
-      );
-      // console.log(req);
-      const res = await req.json();
-      // console.log(res);
-      setNews(res)
-      setLoading(false)
+  const [activity, setActivity] = React.useState('');
+  const [edit, setEdit] = React.useState({});
+  const [todos, setTodos] = React.useState([]);
+  const [msg, setMsg] = React.useState('');
+
+  const generateId = () => Date.now();
+
+  const saveTodoHandler = (event) => {
+    event.preventDefault();
+
+    if (!activity) return setMsg('Nama Aktifitas harus diisi');
+
+    setMsg('')
+
+    if (edit.id) {
+      const updatedTodo = {
+        ... edit,
+        activity,
+        done: false
+      }
+
+      const todoIndex = todos.findIndex((todo) => todo.id === edit.id)
+
+      const updatedTodos = [...todos]
+      
+      updatedTodos[todoIndex] = updatedTodo
+
+      setTodos(updatedTodos)
+      
+      return cancelEditHandler();
     }
-    getData()
-  })
+
+    setTodos([...todos, {
+      id: generateId(),
+      activity,
+      done: false,
+    }]);
+    setActivity('')
+
+    // console.log(todos);
+    
+  }
+
+  const editTodoHandler = (todo) => {
+    // console.log(todo);
+    setEdit(todo);
+    setActivity(todo.activity)
+  }
+
+  const doneTodoHandler = (todo) => {
+    const updatedTodo = {
+      ...todo,
+      done: todo.done ? false : true,
+    }
+
+    const todoIndex = todos.findIndex((currentTodo) => currentTodo.id === todo.id)
+
+    const updatedTodos = [...todos]
+      
+    updatedTodos[todoIndex] = updatedTodo
+
+    setTodos(updatedTodos)
+  }
+
+  const removeTodoHandler = (todoId) => {
+    // console.log('Hapus todo dengan id:', todoId);
+    const filteredTodos = todos.filter((todo) => {
+      return todo.id !== todoId;
+    })
+
+    setTodos(filteredTodos)
+
+    if (edit.id) cancelEditHandler();
+  }
+
+  const cancelEditHandler = () => {
+    setEdit({})
+    setActivity('')
+  }
 
   return (
     <>
-      <h1>Data Fetch</h1>
-      {loading ? ( <i>Loading Data ...</i>
+      <h1>Simple Todo List</h1>
+      {msg && <p style={{ color: 'red' }}>{msg}</p>}
+      <form onSubmit={saveTodoHandler}>
+        <input
+          type="text"
+          placeholder="Nama Aktifikat..."
+          value={activity}
+          onChange={(event) => {setActivity(event.target.value)}}
+        />
+        <button type="submit">
+          {edit.id ? 'Simpan perubahan' : 'Tambah'}
+        </button>
+        {edit.id &&  <button onClick={cancelEditHandler}>Batal</button>}
+      </form>
+      {todos.length > 0 ? (
+        <ul>
+        {todos.map(function (todo) {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.done}
+                onChange={doneTodoHandler.bind(this, todo)}
+              />
+              {todo.activity}
+              ({todo.done ? 'Selesai' : 'Belum Selesai'})
+              <button onClick={editTodoHandler.bind(this, todo)}>Edit</button>
+              <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
+            </li>
+          )
+        })}
+        </ul>  
       ) : (
-        <ol>
-          {news.map((item) => {
-            return <li key={item.id}>{item.title}</li>
-          })}
-        </ol>
-      )}
+          <p><i>Tidak ada todo</i></p>
+      )
+      }
+      
     </>
   )
 }
