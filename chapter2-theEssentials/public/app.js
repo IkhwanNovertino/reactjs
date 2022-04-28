0be5cd0e37d93559ad0b34d69cb65b4ce577bea4
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-04-28 15:08:59 +0800

    sync data fetching response with UI

--- chapter2-theEssentials/public/app.js
+++ chapter2-theEssentials/public/app.js
@@ -1,27 +1,26 @@
 const root = document.querySelector('#root'); // Data fetch
 
 function App() {
-  //Data fetch menggunakan async await
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(true); //Data fetch menggunakan async await
+
   React.useEffect(() => {
     const getData = async () => {
       const req = await fetch('https://api.spaceflightnewsapi.net/v3/blogs'); // console.log(req);
 
-      const res = await req.json();
-      console.log(res);
+      const res = await req.json(); // console.log(res);
+
+      setNews(res);
+      setLoading(false);
     };
 
     getData();
-  }); //Data fetch menggunakan promise
-  // React.useEffect(() => {
-  //   const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-  //     .then((res) => {
-  //       return res.json();
-  //     }).then((response) => {
-  //       return response;
-  //     })
-  // }, [])
-
-  return /*#__PURE__*/React.createElement("h1", null, "Data Fetch");
+  });
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), loading ? /*#__PURE__*/React.createElement("i", null, "Loading Data ...") : /*#__PURE__*/React.createElement("ol", null, news.map(item => {
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id
+    }, item.title);
+  })));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
